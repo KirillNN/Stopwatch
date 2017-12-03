773af0b775f70106d83729e2310cba0031a23d56
kirill <kirill_nn@inbox.ru>
2017-12-03 11:28:44 +0300

    Page 174

--- app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
+++ app/src/main/java/com/hfad/stopwatch/StopwatchActivity.java
@@ -7,13 +7,20 @@ import android.view.View;
 import android.widget.TextView;
 
 public class StopwatchActivity extends Activity {
-    private int seconds = 0;
+    private int seconds;
     private boolean running;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+        } else {
+            seconds = 0;
+            running = false;
+        }
         runTimer();
     }
 
@@ -28,6 +35,12 @@ public class StopwatchActivity extends Activity {
     public void onClickReset(View view) {
         running = false;
         seconds = 0;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
     }
 
     void runTimer() {
